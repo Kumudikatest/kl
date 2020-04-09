6620d3fa7fb5fb96e8afa5864b60a862f0847a7e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-09 16:08:02 +0530

    test

--- kl.js
+++ kl.js
@@ -1,3 +1,6 @@
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
@@ -6,10 +9,28 @@ exports.handler = async (event) => {
         let data = await ddb.scan({
             TableName: "new"
         }).promise();
-console.log(data);
+        console.log(data);
     } catch (err) {
         // error handling goes here
     };
+    datastore.projects.beginTransaction({
+        projectId: process.env.GCP_PROJECT,
+        resource: {
+            transactionOptions: {
+                readWrite: {}
+            }
+        }
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "transaction": "<transaction ID>"
+        }
+        */
+    })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
 
     return { "message": "Successfully executed" };
 };
