61beef19fc3bd316dac12fb90137e061ab5fa216
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-09 09:53:55 +0530

    test

--- kl.js
+++ kl.js
@@ -1,4 +1,28 @@
+let AWS = require('aws-sdk');
+const ses = new AWS.SES();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await ses.sendEmail({
+            Source: "kumudika@adroitlogic.com",
+            Destination: {
+                ToAddresses: ['kumudika@adroitlogic.com']
+            },
+            Message: {
+                Subject: {
+                    Data: "Test"
+                },
+                Body: {
+                    Text: {
+                        Data: ""
+                    }
+                }
+            }
+        }).promise();
+console.log(data);
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
 };
