844beff12251674b2885b5f99a91de4f7b5d2cb6
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-09 10:33:32 +0530

    test

--- kl.js
+++ kl.js
@@ -1,16 +1,12 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const rekognition = new AWS.Rekognition();
 
 exports.handler = async (event) => {
-
     try {
-        let data = await rekognition.detectText({
-            Image: {
-                S3Object: {
-                    Bucket: "cloud9-ktest",
-                    Name: "thisround1"
-                }
-            }
+        let data = await s3.listObjects({
+            Bucket: "cloud9-ktest",
+            MaxKeys: 10
         }).promise();
 console.log(data);
     } catch (err) {
