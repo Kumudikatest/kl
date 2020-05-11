01169db3ce4efbd4d54defe3fda6ce3575ed9255
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-11 11:41:49 +0530

    add trans

--- kl.js
+++ kl.js
@@ -1,4 +1,18 @@
+let AWS = require('aws-sdk');
+const translate = new AWS.Translate();
+
 exports.handler = function (request, response) {
+    translate.translateText({
+        SourceLanguageCode: "auto",
+        TargetLanguageCode: "en",
+        Text: "hola"
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
 
     response.send({ "message": "Successfully executed" });
 }
